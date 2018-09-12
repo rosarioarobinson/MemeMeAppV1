850c96069583d337cf9c93e29605753be4012fd3
rosarioarobinson <rosario.a.robinson@gmail.com>
2018-09-12 11:40:57 -0400

    con't updates

---
+++ MemeMeAppV1/Meme.swift
@@ -0,0 +1,20 @@
+//
+//  Meme.swift
+//  MemeMeAppV1
+//
+//  Created by Rosario A Robinson on 9/12/18.
+//  Copyright © 2018 Rosario Robinson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Meme Struct
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage?
+    var memedImage: UIImage?
+}
+
